8101ee66c4f1204acad3868cc1dcfbd5ad4dc132
Marrwan <Abdulbasitdamilola6@gmail.com>
2022-08-12 19:44:29 +0100

    hmm

--- controllers/index.js
+++ controllers/index.js
@@ -1,27 +1,18 @@
-                // import calculateAge and use it
-                const calculateAge = require('../services/calculateAge');
+// import calculateAge and use it
+const calculateAge = require("../services/calculateAge");
 
-                exports.index = async(req, res) => {
-                    try {
-                        const dob = req.query.dob;
-                        // check if date in not input correctly formatted
-                        // if(!dob.match(/^\d{1,2}-\d{1,2}-\d{4}$/)) {
-                        //     res.status(400).send('Date of birth is not in the correct format');
-                        // }
+// use the calculateAge function
+exports.index = (req, res) => {
+    const dob = req.query.dob;
+    const age = calculateAge(dob);
 
-                    if (!dob) {
-                            res.status(400).json({status: 'error', message:'provide a date of birth'});
-                        } else if (dob.length < 6 ) {
-                    return res.status(400).json({ status: 'error', message: 'Date of birth is not in the correct format' });
-                } 
-                const age = await calculateAge(dob);
-                    if (age < 0) {
-                        return   res.status(400).json({status: 'error', message:'date of birth is in the future'});
-                        } else {
-                        res.status(200).json({status: 'success', age: age})
-                    }
-                } catch (err) {
-                    return  res.status(500).json({status: 'error', message: err.message})
-                    }
-                    }
+    // Check for errors
+    if (!dob) {
+        res.status(400).send("Please provide a date of birth");
+    } else if (age < 0) {
+        res.status(400).send("Please provide a valid date of birth");
+    } else {
+        res.send(age);
+    }
+}
 
