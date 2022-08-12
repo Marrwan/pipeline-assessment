865a0d800ed10f84185ac5ddd883e61c0d1e97c7
Marrwan <Abdulbasitdamilola6@gmail.com>
2022-08-12 19:50:38 +0100

    kill

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,29 @@
 // import calculateAge and use it
 const calculateAge = require("../services/calculateAge");
 
-// use the calculateAge function
-exports.index = (req, res) => {
+exports.index = async (req, res) => {
     const dob = req.query.dob;
-    const age = calculateAge(dob);
-
-    // Check for errors
+    // check if date in not input correctly formatted
+    // if(!dob.match(/^\d{1,2}-\d{1,2}-\d{4}$/)) {
+    //     res.status(400).send('Date of birth is not in the correct format');
+    // }
+  
     if (!dob) {
-        res.status(400).send("Please provide a date of birth");
-    } else if (age < 0) {
-        res.status(400).send("Please provide a valid date of birth");
-    } else {
-        res.send(age);
+      res
+        .json({ status: "error", message: "provide a date of birth" });
+    } else if (dob.length < 6) {
+      return res.json({
+        status: "error",
+        message: "Date of birth is not in the correct format",
+      });
     }
-}
+    const age = await calculateAge(dob);
+    if (age < 0) {
+      return res
+        .json({ status: "error", message: "date of birth is in the future" });
+    }
+  
+    return res.status(200).json({ status: "success", age: age });
+  };
+  
 
--- services/calculateAge.js
+++ services/calculateAge.js
@@ -2,7 +2,8 @@
 
 const calculateAge = (dob) => {
     var today = new Date();
-    var birthDate = new Date(dob);
+    var birthDate = new Date(+dob || dob);
+    console.log({birthDate});
     var age = today.getFullYear() - birthDate.getFullYear();
     var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
